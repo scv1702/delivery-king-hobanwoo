d07b91b5960708d63128afae34c1c233c8a0a0fd
Changyu Shin <scv1702@naver.com>
2022-10-20 20:57:55 +0900

    Feat: Implement update department name trigger and Add some queries

--- sql/create/create_table.sql
+++ sql/create/create_table.sql
@@ -1,57 +1,69 @@
--- Create relation
+drop table store cascade constraints;
+drop table menu cascade constraints;
+drop table department cascade constraints;
+drop table users cascade constraints;
+drop table user_address cascade constraints;
+drop table coupon cascade constraints;
+drop table review cascade constraints;
+drop table orders cascade constraints;
+drop table order_menu cascade constraints;
+drop table contains cascade constraints;
+drop table cooperates cascade constraints;
+
+-- create relations
 CREATE TABLE Store(
     Store_ID NUMBER NOT NULL,
-    Address VARCHAR(50) NOT NULL,
-    Food_Category VARCHAR(10) NOT NULL,
-    Store_Name VARCHAR(20) NOT NULL,
-    Phone_Number VARCHAR(13) NOT NULL,
-    Description VARCHAR(100),
+    Address VARCHAR(1000) NOT NULL,
+    Food_Category VARCHAR(1000) NOT NULL,
+    Store_Name VARCHAR(1000) NOT NULL,
+    Phone_Number VARCHAR(1000) NOT NULL,
+    Description VARCHAR(1000),
     Delivery_Fee NUMBER NOT NULL,
-    Image VARCHAR(10),
+    Image VARCHAR(1000),
     Business_Hour NUMBER NOT NULL,
-    PRIMARY KEY(Store_ID)
+    PRIMARY KEY(Store_ID),
     UNIQUE(Address)
 );
 
 CREATE TABLE Menu(
     Menu_ID NUMBER NOT NULL,
     Store_ID NUMBER NOT NULL,
-    Mname VARCHAR(15) NOT NULL,
-    Description VARCHAR(100),
-    Image VARCHAR(10),
+    Mname VARCHAR(1000) NOT NULL,
+    Description VARCHAR(1000),
+    Image VARCHAR(1000),
     Price NUMBER NOT NULL,
     PRIMARY KEY(Menu_ID)
 );
 
 CREATE TABLE Department(
-    Dname VARCHAR(15) NOT NULL,
+    Dname VARCHAR(1000) NOT NULL,
     PRIMARY KEY(Dname)
 );
 
-CREATE TABLE User(
+CREATE TABLE Users(
     User_ID NUMBER NOT NULL,
-    User_Name VARCHAR(15) NOT NULL, --추가
-    Dname VARCHAR(15) NOT NULL,
-    Password VARCHAR(10) NOT NULL,
-    Phone_Number VARCHAR(13) NOT NULL,
-    Membership_Tier VARCHAR(5) NOT NULL, --이거 derived로 하면?
-    PRIMARY KEY(User_ID)
+    User_Name VARCHAR(1000) NOT NULL,
+    Dname VARCHAR(1000) NOT NULL,
+    Password VARCHAR(1000) NOT NULL,
+    Phone_Number VARCHAR(1000) NOT NULL,
+    --Membership_Tier VARCHAR(5) NOT NULL,
+    PRIMARY KEY(User_ID),
     UNIQUE(Phone_Number)
 );
 
 CREATE TABLE User_Address(
     User_ID NUMBER NOT NULL,
-    UAddress VARCHAR(50) NOT NULL,
+    UAddress VARCHAR(1000) NOT NULL,
     PRIMARY KEY(User_ID, UAddress)
 );
 
 CREATE TABLE Coupon(
     Coupon_ID NUMBER NOT NULL,
     User_ID NUMBER NOT NULL,
-    Discount_Amount VARCHAR(5) NOT NULL,
+    Discount_Amount VARCHAR(1000) NOT NULL,
     Expiration_Date DATE NOT NULL,
-    Minimum_Order_Amount VARCHAR(10) NOT NULL,
-    State VARCHAR(5) NOT NULL,
+    Minimum_Order_Amount VARCHAR(1000) NOT NULL,
+    State VARCHAR(1000) NOT NULL,
     PRIMARY KEY(Coupon_ID)
 );
 
@@ -60,17 +72,17 @@ CREATE TABLE Review(
     User_ID NUMBER NOT NULL,
     Store_ID NUMBER NOT NULL,
     Star_Rating NUMBER NOT NULL,
-    Comment VARCHAR(100),
+    Comments VARCHAR(1000),
     Created_At DATE NOT NULL,
     PRIMARY KEY(Review_ID)
 );
 
-CREATE TABLE Order(
+CREATE TABLE Orders(
     Order_ID NUMBER NOT NULL,
     User_ID NUMBER NOT NULL,
     Store_ID NUMBER NOT NULL,
-    Payment VARCHAR(5) NOT NULL,
-    State VARCHAR(5) NOT NULL,
+    Payment VARCHAR(1000) NOT NULL,
+    State VARCHAR(1000) NOT NULL,
     Order_Date DATE NOT NULL,
     PRIMARY KEY(Order_ID)
 );
@@ -78,22 +90,22 @@ CREATE TABLE Order(
 CREATE TABLE Order_Menu(
     Order_Menu_ID NUMBER NOT NULL,
     Order_ID NUMBER NOT NULL,
-    Menu_Name VARCHAR(15) NOT NULL,
-    Menu_Image VARCHAR(10),
+    Menu_Name VARCHAR(1000) NOT NULL,
+    Menu_Image VARCHAR(1000),
     Menu_Price NUMBER NOT NULL,
     Quantity NUMBER NOT NULL,
-    PRIMARY KEY(Order_Menu_ID, Menu_Name)
+    PRIMARY KEY(Order_Menu_ID)
 );
 
 CREATE TABLE Contains(
-    Menu_ID NUMBER NOT NULL,
+    Order_Menu_ID NUMBER NOT NULL,
     Review_ID NUMBER NOT NULL,
-    PRIMARY KEY(Menu_ID, Review_ID)
+    PRIMARY KEY(Order_Menu_ID, Review_ID)
 );
 
 CREATE TABLE Cooperates(
     Store_ID NUMBER NOT NULL,
-    Dname VARCHAR(15) NOT NULL,
+    Dname VARCHAR(1000) NOT NULL,
     PRIMARY KEY(Store_ID, Dname)
 );
 
@@ -107,28 +119,28 @@ ON DELETE CASCADE;
 ALTER TABLE Review ADD FOREIGN KEY (Store_ID) REFERENCES Store(Store_ID)
 ON DELETE CASCADE;
 
-ALTER TABLE Order ADD FOREIGN KEY (Store_ID) REFERENCES Store(Store_ID)
+ALTER TABLE Orders ADD FOREIGN KEY (Store_ID) REFERENCES Store(Store_ID)
 ON DELETE SET NULL;
 
 ALTER TABLE Contains ADD FOREIGN KEY (Order_Menu_ID) REFERENCES Order_Menu(Order_Menu_ID);
 
-ALTER TABLE User ADD FOREIGN KEY (Dname) REFERENCES Department(Dname)
+ALTER TABLE Users ADD FOREIGN KEY (Dname) REFERENCES Department(Dname)
 ON DELETE SET NULL;
 
 ALTER TABLE Cooperates ADD FOREIGN KEY (Dname) REFERENCES Department(Dname)
 ON DELETE SET NULL;
 
-ALTER TABLE Order ADD FOREIGN KEY (User_ID) REFERENCES User(User_ID);
+ALTER TABLE Orders ADD FOREIGN KEY (User_ID) REFERENCES Users(User_ID);
 
-ALTER TABLE Review ADD FOREIGN KEY (User_ID) REFERENCES User(User_ID);
+ALTER TABLE Review ADD FOREIGN KEY (User_ID) REFERENCES Users(User_ID);
 
-ALTER TABLE Coupon ADD FOREIGN KEY (User_ID) REFERENCES User(User_ID);
+ALTER TABLE Coupon ADD FOREIGN KEY (User_ID) REFERENCES Users(User_ID);
 
-ALTER TABLE User_Address ADD FOREIGN KEY (User_ID) REFERENCES User(User_ID)
+ALTER TABLE User_Address ADD FOREIGN KEY (User_ID) REFERENCES Users(User_ID)
 ON DELETE CASCADE;
 
 ALTER TABLE Contains ADD FOREIGN KEY (Review_ID) REFERENCES Review(Review_ID)
 ON DELETE CASCADE;
 
-ALTER TABLE Order_Menu ADD FOREIGN KEY (Order_ID) REFERENCES Order(Order_ID)
+ALTER TABLE Order_Menu ADD FOREIGN KEY (Order_ID) REFERENCES Orders(Order_ID)
 ON DELETE CASCADE;
