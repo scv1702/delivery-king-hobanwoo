5eca0fc7ccbfa34b4eda82a2025a6cde2e5952ad
Changyu Shin <scv1702@naver.com>
2022-11-13 04:33:06 +0900

    Refactor: Refactored

--- Phase3/src/mentTEST.java
+++
@@ -1,7 +0,0 @@
-public class mentTEST {
-    public static void main(String[] args) {
-        System.out.print(
-                "┌---------------------------------------------------------------┐\n" +
-                "│       \t\t\t\t 주문 접수 완료 ! \t\t\t            │\n" );
-    }
-}
